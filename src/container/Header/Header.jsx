16533bf21e3054a70bf4006dafa1c68ecaabab50
Maruf Al Hossain <pmaruf42@gmail.com>
2022-06-01 11:23:34 +0600

    header section complete

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 import './Header.css';
+import {SubHeading} from "../../components";
+import {images} from '../../constants';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour"/>
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{margin: '2rem 0'}}>
+        auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue ac consequat, aliquam molestie lectus eu. Congue iaculis integer curabitur semper sit nunc.
+      </p>
+      <button className="custom__button">Explore Menu</button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header"/>
+    </div>
   </div>
 );
 
